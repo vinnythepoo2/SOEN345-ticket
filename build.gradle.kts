e3f39c5cff12fce39250b6c0e22ae74f96c1b2b4
vincentds2 <v_deser@live.concordia.ca>
2026-04-06 14:29:25 -0400

    set up sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,9 @@ plugins {
 sonar {
     properties {
         property("sonar.projectName", "SOEN345-ticket")
-        property("sonar.projectKey", "SOEN345-ticket")
+        property("sonar.projectKey", "vinnythepoo2_SOEN345-ticket")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.organization", "your-organization-key")
+        property("sonar.organization", "vinnythepoo2")
         property("sonar.token", System.getenv("SONAR_TOKEN") ?: "")
         property("sonar.android.lint.report", "${project.layout.buildDirectory.get()}/reports/lint-results-debug.xml")
         property("sonar.coverage.jacoco.xmlReportPaths", "${project.layout.buildDirectory.get()}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
